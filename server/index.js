0f112080152557da0ff7b23454b40ed4e1e3dd4d
Miguel Baniqued <bmiguelcedric@gmail.com>
2023-10-05 16:56:30 -0700

    New Setup
    Previous commit was based on a YouTube tutorial from 5 years ago. There could be outdated information that will not work today. Switched to a new YouTube tutorial.

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone'
+
+// Server setup
+const server = new ApolloServer({
+
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 }
+});
+
+console.log(`Server ready at ${url}`);
